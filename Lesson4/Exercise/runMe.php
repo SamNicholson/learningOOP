46bd616fdd94584ccd0493391cf4e497bef162b1
SamNicholson <snicholson@workanyware.co.uk>
2015-10-28 16:11:38 +0000

    Added autoloader to excercise 4

--- Lesson4/Exercise/runMe.php
+++ Lesson4/Exercise/runMe.php
@@ -1,7 +1,9 @@
 <?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
 /**
  * Exercise 4 -
  * Implement your previously made preference class with the interface provided
  * this will ensure that there is backwards compatibility
- * with any existing functionalit
+ * with any existing functionality
  */
